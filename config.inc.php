68371eafba11295a0fc9e6737f16f0ab1dafda8b
Elias Müller <elias.mueller@rwth-aachen.de>
2012-11-21 11:06:34 +0100

    Updated main config file: new version, removed useless config values.

--- config.inc.php
+++ config.inc.php
@@ -19,7 +19,7 @@ return array(
 	// site information
 	'site'=>array(
 		'title'=>'Poker',
-		'version'=>'0.1 alpha',
+		'version'=>'0.2 alpha',
 		'has_frontend'=>false
 	),
 	// Navigation: Menu Structure for the backend
@@ -34,55 +34,5 @@ return array(
 		'database'=>'poker',
 		'user'=>'root',
 		'password'=>'x1y3z5a1'
-	),
-	// Invoice / Stat: prices for special rooms 
-	'prices'=>array(
-		'saal'=>150,
-		'keller'=>80,
-		'schänke'=>80
-	),
-	// Mail: server settings
-	'mail'=>array(
-		'user'=>'m01ca773',
-		'password'=>'YW6D9nc4DAxvLDfx',
-		'host'=>'jugendburg-balduinstein.de',
-		'smtp'=>'relay-auth.rwth-aachen.de',
-		'smtp_port'=>465,
-		'smtp_user'=>'em554929+rwth-aachen.de',
-		'smtp_password'=>'9wtCu#va2',
-	),
-	// pdf: margins, fonts and other format options
-	'pdf'=>array(
-		'page'=>array(
-			'format'=>'A4',
-			'orientation'=>'P',
-		),
-		'creator'=>'TCPDF',
-		'author'=>'Jugendburg Balduinstein',
-		'unit'=>'mm',
-		'margin'=>array(
-			'header'=>60,
-			'footer'=>10,
-			'top'=>60,
-			'bottom'=>25,
-			'left'=>25,
-			'right'=>25
-		),
-		'font'=>array(
-			'main'=>'rotis_serif',
-			'header'=>'rotis_sans_light',
-		),
-		'size'=>array(
-			'main'=>12,
-			'header'=>13,
-			'title'=>14,
-		),
-		'header'=>array(
-			'text'=>"auf der burg
-65558 balduinstein
-telefon: (06432) 83910
-email: kontakt@jugendburg-balduinstein.de",
-			'logo'=>'images/logo_invoice.png'
-		)
 	),
 );
